1ecfbd614675eb5d87d1bfe1dee5ca2fd877ebff
Jonathan Duque <johndduque@gmail.com>
2020-05-07 23:02:19 -0400

    client side

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+// Here's to 100 commits and a badass project
+import React from "react";
+import ReactGA from "react-ga";
+import { Layout, Divider } from "antd";
+import { BellTwoTone } from "@ant-design/icons";
+
+import Window from "./components/Window";
+import Fields from "./components/Fields";
+
+import "./App.css";
+
+//RE-ENABLE ONCE FIGURED OUT
+
+//ReactGA.initialize("UA-164161545-1");
+//ReactGA.pageview(window.location.pathname + window.location.search);
+
+const { Header, Content, Footer } = Layout;
+
+function App() {
+  return (
+    <div>
+      <Layout className="layout">
+        <Header style={{ color: "#FFF", fontSize: "16px" }}>Zoom Notifs</Header>
+        <Content>
+          <div
+            className="site-layout-content"
+            style={{ padding: "25px 50px 85px 50px" }}
+          >
+            <h1>
+              <BellTwoTone /> Receive email reminders with Zoom invites to your
+              classes
+            </h1>
+            {/* Add Product Info */}
+            <Window />
+            <Divider>Sign Up</Divider>
+            <Fields />
+          </div>
+        </Content>
+      </Layout>
+      <Footer
+        style={{
+          textAlign: "center",
+          width: "100%",
+          position: "fixed",
+          bottom: "0",
+        }}
+      >
+        Built with ❤️ by Jonathan Duque wishing everyone well in these uncertain
+        times
+      </Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Fields.js
@@ -0,0 +1,155 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Form, Input, Button, Tooltip, message as Message } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
+
+import Success from "./Success";
+
+import "./Fields.css";
+
+const layout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 14 },
+};
+const tailLayout = {
+  wrapperCol: { span: 0 },
+};
+
+export class Fields extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      url: "",
+      loading: false,
+      showSuccessModal: false,
+      classesWithoutLinks: [],
+    };
+  }
+
+  onFinish = () => {
+    // Set state for loading for button spinner
+    this.setState({ loading: true });
+
+    const { email, url } = this.state;
+    const key = "/shared/schedule/";
+
+    const keyIndex = url.indexOf(key);
+    const idIndex = keyIndex + key.length;
+
+    const schedulerId = url.substring(idIndex);
+
+    // Retrieve schedule info from API
+    axios
+      .get(`/api/schedule?email=${email}&id=${schedulerId}`)
+      .then((res) => {
+        this.setState({ loading: false });
+        if (res.data.success === true) {
+          // Only load success modal prompt when there is at least one unlinked class
+          if (res.data.unlinkedClasses.length > 0) {
+            // Pop up with success modal
+            this.setState({
+              showSuccessModal: true,
+              classesWithoutLinks: res.data.unlinkedClasses,
+            });
+          } else {
+            Message.success(`Successfully registered with ${email}`);
+          }
+        } else {
+          const errorMsg =
+            res && res.data.message && res.data.message.length > 0
+              ? res.data.message
+              : "Unable to sign up at the moment. Please try again later";
+          Message.error(errorMsg);
+        }
+      })
+      .catch(() => {
+        this.setState({ loading: false });
+        Message.error(
+          "Unable to sign up at the moment. Please try again later"
+        );
+      });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Form
+          {...layout}
+          style={{ marginTop: "24px" }}
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={this.onFinish}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            style={{ textAlign: "left" }}
+            rules={[
+              { required: true, message: "Email field cannot be empty" },
+              { type: "email", message: "Please enter a valid email" },
+            ]}
+          >
+            <Input
+              placeholder="jd849@cornell.edu"
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+          </Form.Item>
+          <Form.Item
+            label="Schedule URL"
+            name="url"
+            style={{ textAlign: "left" }}
+            rules={[
+              { required: true, message: "URL field cannot be empty" },
+              {
+                pattern: /(https?:\/\/)?(www\.)?classes\.cornell\.edu\/shared\/schedule\/sp20\/.......+/gi,
+                message: "Please enter a valid Spring 2020 shared schedule URL",
+              },
+            ]}
+          >
+            <Input
+              placeholder="https://classes.cornell.edu/shared/schedule/SP20/92eac951cf5b329be2522a9829421833"
+              onChange={(e) => this.setState({ url: e.target.value })}
+              suffix={
+                <Tooltip
+                  title={
+                    <p style={{ margin: "0" }}>
+                      Visit{" "}
+                      <a
+                        href="https://classes.cornell.edu/scheduler/roster/SP20"
+                        target="_blank"
+                      >
+                        Scheduler
+                      </a>{" "}
+                      and click 'Share'
+                    </p>
+                  }
+                >
+                  <InfoCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
+                </Tooltip>
+              }
+            />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              loading={this.state.loading}
+            >
+              {this.state.loading ? "Loading" : "Submit"}
+            </Button>
+          </Form.Item>
+        </Form>
+        <Success
+          email={this.state.email}
+          visible={this.state.showSuccessModal}
+          classes={this.state.classesWithoutLinks}
+          hideSuccess={() => this.setState({ showSuccessModal: false })}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Fields;
