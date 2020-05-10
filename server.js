d2445fd07d64249a5533da2a919d32405dd5ad34
Jonathan Duque <johndduque@gmail.com>
2020-05-10 15:29:34 -0400

    add postbuild script

--- server.js
+++ server.js
@@ -100,9 +100,9 @@ app.get("/test", (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-//app.get("*", (req, res) => {
-//  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-//});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 3333;
 
